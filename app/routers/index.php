1b0bd7388f1c61609076f4f9ce0bb19b691000d5
MN <bmdaetl@gmail.com>
2025-11-18 13:14:39 +0100

    Traduction des helpers en Helpers

--- app/routers/index.php
+++ app/routers/index.php
@@ -5,9 +5,4 @@
 // CTLR: pagesController
 // ACTION: home
 
-// VERSION 1
-// $pagesController = new \App\Controllers\PagesController();
-// $pagesController->homeAction($conn);
-
-// VERSION 2
 \App\Controllers\PagesController::homeAction($conn);
--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -3,7 +3,11 @@
     <?php foreach ($books as $book): ?>
         <li>
             <?php echo $book['title']; ?>
-            <?php echo $book['resume']; ?>
+            <!-- Tronquer le resume à 30 caractères
+                1. Transformer notre Helpers en classe
+                2. Changer le PSR-4
+                3. Composer dump-autoload -->
+            <?php echo \Core\Helpers::truncate($book['resume'], 30); ?>
         </li>
     <?php endforeach; ?>
 </ul>
