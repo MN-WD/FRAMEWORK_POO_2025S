378411bd6a5a7bc2187fb803ff8950e3f224f942
MN <bmdaetl@gmail.com>
2025-11-18 09:36:44 +0100

    Structure MVC de base

---
+++ app/config/params.php
@@ -0,0 +1,12 @@
+<?php
+
+// Initialisation des zones dynamiques
+$title = 'Books & Authors';
+$content = '';
+
+
+// Paramètres de connexion à la DB
+define('DB_HOST', '127.0.0.1:3306');
+define('DB_NAME', 'book_hunter_2025');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
